2a769b7fb42387d56f7250f9d4bc29b08acff7fc
hjnoda <90017645+hjnoda@users.noreply.github.com>
2022-10-26 08:07:27 +0900

    投稿調整

--- public_html/wp/wp-content/themes/politics/sidebar.php
+++ public_html/wp/wp-content/themes/politics/sidebar.php
@@ -1,14 +1,14 @@
 <div class="sidebar">
-	<ul class="list-group list-group-flush">
-		<li class="list-group-item title">カテゴリー</li>
-		<?php get_category_list(); ?>
-	</ul>
-	<ul class="list-group list-group-flush">
-		<li class="list-group-item title">タグ</li>
-		<?php get_tags_list(); ?>
-	</ul>
-	<ul class="list-group list-group-flush">
-		<li class="list-group-item title">アーカイブ</li>
-		<?php get_archive_list(); ?>
-	</ul>
+  <div class="list-group-item title">カテゴリー</div>
+  <ul class="list-group list-group-flush">
+    <?php get_category_list(); ?>
+  </ul>
+  <div class="list-group-item title">アーカイブ</div>
+  <ul class="list-group list-group-flush">
+    <?php get_archive_list(); ?>
+  </ul>
+  <!-- <div class="list-group-item title">タグ</div>
+  <ul class="list-group tag-lists">
+    <?php //get_tags_list(); ?>
+  </ul> -->
 </div>
